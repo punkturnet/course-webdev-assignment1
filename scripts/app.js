c8994600f99c317dccee13066f599646795cb942
Geiri <git@geiri.net>
2016-09-28 21:57:59 +0000

    Let's get this stuff to github

---
+++ scripts/app.js
@@ -0,0 +1,30 @@
+(function(){
+  'use strict';
+  angular.module('LunchApp', [])
+  .controller('LunchController', LunchController);
+
+  LunchController.$inject = ['$scope'];
+  function LunchController($scope){
+    $scope.message = "";
+    $scope.dishes = "";
+    $scope.statusClass="";
+
+    $scope.CheckDishes = function(){
+      if($scope.dishes !== ""){
+        var dishesCount = $scope.dishes.split(',').length;
+        if(dishesCount <= 3){
+          $scope.message = "Enjoy!";
+          $scope.statusClass = "alert-success";
+        }
+        else if(dishesCount > 3){
+          $scope.message = "Too much!"
+          $scope.statusClass = "alert-warning";
+        }
+      }
+      else {
+        $scope.message = "Please enter data first";
+        $scope.statusClass = 'alert-error';
+      }
+    };
+  }
+})()

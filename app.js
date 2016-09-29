560a1481ca0ef81d9e4228211191a420fb9c2879
Ásgeir Freyr Kristinsson <asgeir.kristinsson@advania.is>
2016-09-29 11:31:00 +0000

    .

--- app.js
+++ app.js
@@ -1,38 +1,12 @@
-(function() {
-  'use strict';
+(function () {
+'use strict';
 
-  angular.module('LunchApp', [])
-  .controller('LunchController', LunchController);
+angular.module('LunchApp', [])
+.controller('LunchController', LunchController);
 
-  LunchController.$inject = ['$scope'];
+LunchController.$inject = ['$scope'];
+function LunchController($scope) {
+  
+}
 
-  function LunchController($scope)
-  {
-    $scope.message = "";
-    $scope.dishes = "";
-    $scope.statusClass = "";
-
-    $scope.CheckDishes = function(){
-      if($scope.dishes !== ""){
-        
-        var dishesCount = $scope.dishes.split(',').length;
-        
-        if(dishesCount <= 3)
-        {
-          $scope.message = "Enjoy!";
-          $scope.statusClass = "alert-success";
-        }
-        else if(dishesCount > 3)
-        {
-          $scope.message = "Too much!"
-          $scope.statusClass = "alert-warning";
-        }
-      }
-      else 
-      {
-        $scope.message = "Please enter data firstt";
-        $scope.statusClass = 'alert-danger';
-      }
-    };
-  }
 })();

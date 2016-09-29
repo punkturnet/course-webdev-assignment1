60733fd836e045231825c0b9c5564e3218c34f15
Ásgeir Freyr Kristinsson <asgeir.kristinsson@advania.is>
2016-09-29 10:44:28 +0000

    ...

--- app.js
+++ app.js
@@ -1,27 +1,33 @@
 (function(){
   'use strict';
-  angular.module("LunchApp", [])
-  .controller("LunchController", LunchController);
+  angular.module("LunchApp", []).controller("LunchController", LunchController);
 
   LunchController.$inject = ["$scope"];
-  function LunchController($scope){
+
+  function LunchController($scope)
+  {
     $scope.message = "";
     $scope.dishes = "";
-    $scope.statusClass="";
+    $scope.statusClass = "";
 
     $scope.CheckDishes = function(){
       if($scope.dishes !== ""){
+        
         var dishesCount = $scope.dishes.split(',').length;
-        if(dishesCount <= 3){
+        
+        if(dishesCount <= 3)
+        {
           $scope.message = "Enjoy!";
           $scope.statusClass = "alert-success";
         }
-        else if(dishesCount > 3){
+        else if(dishesCount > 3)
+        {
           $scope.message = "Too much!"
           $scope.statusClass = "alert-warning";
         }
       }
-      else {
+      else 
+      {
         $scope.message = "Please enter data firstt";
         $scope.statusClass = 'alert-danger';
       }

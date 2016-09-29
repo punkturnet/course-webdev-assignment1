548bc328f26afd2e6d0a75b81f5c3b1e93c93b99
Ásgeir Freyr Kristinsson <asgeir.kristinsson@advania.is>
2016-09-29 13:15:48 +0000

    Final submission

--- app.js
+++ app.js
@@ -5,33 +5,47 @@ angular.module('LunchApp', [])
 .controller('LunchController', LunchController);
 
 LunchController.$inject = ['$scope'];
+
 function LunchController($scope) {
   $scope.message = "";
   $scope.dishes = "";
   $scope.statusClass = "";
+  $scope.inputClass = "";
+  $scope.dishesCount = 0;
 
   $scope.CheckDishes = function(){
+    $scope.dishesCount = 0;
     if($scope.dishes !== ""){
-      
-      var dishesCount = $scope.dishes.split(',').length;
-      
-      if(dishesCount <= 3)
+      var dishes = $scope.dishes.split(',');
+
+      for (var i = 0; i < dishes.length; i++) {
+        if(dishes[i] && dishes[i].length > 0){
+          $scope.dishesCount++;
+        }
+      };
+
+      if($scope.dishesCount <= 3)
       {
         $scope.message = "Enjoy!";
-        $scope.statusClass = "alert-success";
+        SetStatus('success');
       }
-      else if(dishesCount > 3)
+      else if($scope.dishesCount > 3)
       {
         $scope.message = "Too much!"
-        $scope.statusClass = "alert-warning";
+        SetStatus('warning');
       }
     }
     else 
     {
-      $scope.message = "Please enter data firstt";
-      $scope.statusClass = 'alert-danger';
+      $scope.message = "Please enter data first";
+      SetStatus('danger');
     }
   };
+
+  function SetStatus(status){
+      $scope.statusClass = "alert-" + status;
+      $scope.inputClass = "has-" + ((status === 'success' || status === 'warning') ? 'success' : 'error');
+  }
 }
 
 })();
